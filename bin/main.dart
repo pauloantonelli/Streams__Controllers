d16818a8bfd0b872c5872e51d282fcb19b2a09d6
pauloantonelli <pauloantonelli@hotmail.com>
2019-12-29 10:40:05 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:rxdart/rxdart.dart';
+
+void main() {
+  PublishSubject primeiroFluxo = new PublishSubject();
+
+  primeiroFluxo.stream.listen((data) {
+    print(data);
+  });
+
+  primeiroFluxo.sink.add(1);
+  primeiroFluxo.sink.add(2);
+  primeiroFluxo.sink.add(3);
+}
