ff3a52ac9f109760fb6673ac08bbcb38234b9d79
pauloantonelli <pauloantonelli@hotmail.com>
2019-12-29 12:31:06 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,12 @@
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  BehaviorSubject primeiroFluxo = new BehaviorSubject();
+  ReplaySubject primeirFluxo = new ReplaySubject();
 
-  primeiroFluxo.sink.add(1);
-  primeiroFluxo.sink.add(2);
-  primeiroFluxo.sink.add(3);
+  primeirFluxo.sink.add(1);
+  primeirFluxo.stream.listen((data) => print('print $data'));
+  primeirFluxo.sink.add(2);
+  primeirFluxo.sink.add(3);
 
-  primeiroFluxo.stream.listen((data) {
-    print(data);
-  });
-  primeiroFluxo.sink.add(4);
-  print(primeiroFluxo.value);
+  print('values: ${primeirFluxo.values}');
 }
