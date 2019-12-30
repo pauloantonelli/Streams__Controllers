cc72e41f60188e94a6145458ebab5200d4b68e10
pauloantonelli <pauloantonelli@hotmail.com>
2019-12-30 17:48:25 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,30 @@
+import 'dart:async';
+
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  Stream.fromIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-      .interval(Duration(seconds: 1))
-      .listen(print);
+  StreamController streamController = StreamController.broadcast();
+
+  streamController.stream.listen((data) => print('stream sem map: $data'));
+  streamController.stream
+      .map((data) {
+        return data * 2;
+      })
+      .where((data) => data > 4)
+      .listen((data) => print('stream transformada: $data'));
+  streamController.sink.add(1);
+  streamController.sink.add(2);
+  streamController.sink.add(3);
+  streamController.sink.add(4);
+  streamController.sink.add(5);
+
+  Iterable listaMapeada = [1, 2, 3, 4, 5];
+  print(listaMapeada.map((data) {
+    return data * 2;
+  }).where((data) => data > 3));
+
+  Iterable caixaAlta = ['a', 'b', 'c', 'd'];
+  streamController.sink.add(caixaAlta.map((data) {
+    return data.toUpperCase();
+  }));
 }
