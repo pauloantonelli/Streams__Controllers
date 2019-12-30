20ceb9a447ff86175496f44a70261faab21f7508
pauloantonelli <pauloantonelli@hotmail.com>
2019-12-30 12:04:59 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,7 @@
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  BehaviorSubject<int> b = new BehaviorSubject<int>.seeded(12);
-  Stream<String> stream = new Stream<String>.value('abc');
-  // b.sink.add(1);
-  b.stream.listen(print);
-
-  stream.listen(print);
+  Stream.fromIterable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+      .interval(Duration(seconds: 1))
+      .listen(print);
 }
