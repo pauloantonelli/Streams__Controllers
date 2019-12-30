36e17b2679c212a5fd1d7f84ebf4b655288db820
pauloantonelli <pauloantonelli@hotmail.com>
2019-12-30 11:16:43 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -1,22 +1,10 @@
-import 'dart:async';
-
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  Stream stream = Stream.value(
-      'a'); // emite um valor, que depois de escutado morre a instancia da stream
-  stream.listen(
-    print,
-    onDone: () => print('Terminei de receber os dados do observable!'),
-  );
+  BehaviorSubject<int> b = new BehaviorSubject<int>.seeded(12);
+  Stream<String> stream = new Stream<String>.value('abc');
+  // b.sink.add(1);
+  b.stream.listen(print);
 
-  BehaviorSubject behavior = new BehaviorSubject();
-  StreamSubscription subscription = behavior.listen(print);
-  behavior.sink.add(10);
-  behavior.sink.add(20);
-  subscription.pause();
-  behavior.sink.add(30);
-  subscription.resume();
-  subscription
-      .onData((dados) => print('sobreescrevi o .listen, dados: $dados'));
+  stream.listen(print);
 }
