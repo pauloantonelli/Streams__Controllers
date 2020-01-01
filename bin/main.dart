84ecf0b31f4a8337eebf2ab6e51a07ef17ad15cb
pauloantonelli <pauloantonelli@hotmail.com>
2020-01-01 15:37:27 -0300

    fix

--- bin/main.dart
+++ bin/main.dart
@@ -1,14 +1,14 @@
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  BehaviorSubject primeiroFluxo = new BehaviorSubject();
-
-  primeiroFluxo.sink.add(1);
-  primeiroFluxo.sink.add(2);
-  primeiroFluxo.sink.add(3);
+  BehaviorSubject primeiroFluxo = new BehaviorSubject.seeded(0);
 
   primeiroFluxo.stream.listen((data) {
     print(data);
   });
+  primeiroFluxo.sink.add(1);
+  primeiroFluxo.sink.add(2);
+  primeiroFluxo.sink.add(3);
   primeiroFluxo.sink.add(4);
+  print('behavior value: ${primeiroFluxo.value}');
 }
