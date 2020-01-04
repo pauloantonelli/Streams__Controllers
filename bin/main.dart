115c122b03850e2c50e938aa696e870378b71c11
pauloantonelli <pauloantonelli@hotmail.com>
2020-01-04 14:21:45 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -4,21 +4,22 @@ import 'package:rxdart/rxdart.dart';
 
 void main() {
   StreamController stc = new StreamController.broadcast();
-  Iterable lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  final listaFiltradaTake = lista.takeWhile((data) => data < 3);
-  print('takeX: $listaFiltradaTake');
-  final listaFiltradaSkip = lista.skipWhile((data) => data < 3);
-  print('skip: $listaFiltradaSkip');
+  Iterable lista = [
+    [1, 2, 3],
+    [4],
+    [5, 6, 7],
+    [8, 9],
+    [10]
+  ].expand((data) => data);
+  print('array: $lista');
 
   stc.stream
-      .takeWhile((data) => data < 3)
-      .listen((data) => print('take stream: $data'));
-  stc.stream
-      .skipWhile((data) => data < 4)
-      .listen((data) => print('skip stream: $data'));
-  stc.sink.add(1);
-  stc.sink.add(2);
-  stc.sink.add(3);
-  stc.sink.add(4);
-  stc.sink.add(5);
+      .expand((data) => data)
+      .map((data) => data * 2)
+      .listen((onData) => print('Stream $onData'));
+  stc.sink.add([1, 2, 3]);
+  stc.sink.add([4]);
+  stc.sink.add([5, 6, 7]);
+  stc.sink.add([8, 9]);
+  stc.sink.add([10]);
 }
