1259cc6aacd090e5081a308adb9956ed2cc8fcd2
pauloantonelli <pauloantonelli@hotmail.com>
2020-01-04 12:08:36 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,18 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  final lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  final listaFiltrada = lista.where((data) => data > 3);
-  print(listaFiltrada);
+  StreamController stc = new StreamController.broadcast();
+  Iterable lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final listaFiltradaTake = lista.take(3);
+  print('take: $listaFiltradaTake');
+  final listaFiltradaSkip = lista.skip(3);
+  print('skip: $listaFiltradaSkip');
+
+  stc.stream.take(2).listen((data) => print('take: $data'));
+  stc.stream.skip(3).listen((data) => print('skip: $data'));
+  stc.sink.add(1);
+  stc.sink.add(2);
+  stc.sink.add(3);
+  stc.sink.add(4);
+  stc.sink.add(5);
 }
