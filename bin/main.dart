1dfed3fc9e86d3196f939d40e02b0403effaf749
pauloantonelli <pauloantonelli@hotmail.com>
2020-01-04 13:48:50 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -5,13 +5,17 @@ import 'package:rxdart/rxdart.dart';
 void main() {
   StreamController stc = new StreamController.broadcast();
   Iterable lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  final listaFiltradaTake = lista.take(3);
-  print('take: $listaFiltradaTake');
-  final listaFiltradaSkip = lista.skip(3);
+  final listaFiltradaTake = lista.takeWhile((data) => data < 3);
+  print('takeX: $listaFiltradaTake');
+  final listaFiltradaSkip = lista.skipWhile((data) => data < 3);
   print('skip: $listaFiltradaSkip');
 
-  stc.stream.take(2).listen((data) => print('take: $data'));
-  stc.stream.skip(3).listen((data) => print('skip: $data'));
+  stc.stream
+      .takeWhile((data) => data < 3)
+      .listen((data) => print('take stream: $data'));
+  stc.stream
+      .skipWhile((data) => data < 4)
+      .listen((data) => print('skip stream: $data'));
   stc.sink.add(1);
   stc.sink.add(2);
   stc.sink.add(3);
