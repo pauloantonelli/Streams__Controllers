8369d809d15adaa37bd0acda1cc07566df95f929
pauloantonelli <pauloantonelli@hotmail.com>
2020-01-04 11:35:02 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -3,28 +3,7 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  StreamController streamController = StreamController.broadcast();
-
-  streamController.stream.listen((data) => print('stream sem map: $data'));
-  streamController.stream
-      .map((data) {
-        return data * 2;
-      })
-      .where((data) => data > 4)
-      .listen((data) => print('stream transformada: $data'));
-  streamController.sink.add(1);
-  streamController.sink.add(2);
-  streamController.sink.add(3);
-  streamController.sink.add(4);
-  streamController.sink.add(5);
-
-  Iterable listaMapeada = [1, 2, 3, 4, 5];
-  print(listaMapeada.map((data) {
-    return data * 2;
-  }).where((data) => data > 3));
-
-  Iterable caixaAlta = ['a', 'b', 'c', 'd'];
-  streamController.sink.add(caixaAlta.map((data) {
-    return data.toUpperCase();
-  }));
+  final lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final listaFiltrada = lista.where((data) => data > 3);
+  print(listaFiltrada);
 }
