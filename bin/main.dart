10c2c65f18320ceac2e180fd07e55f1eafaf02b3
pauloantonelli <pauloantonelli@hotmail.com>
2020-01-11 16:11:08 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -3,5 +3,12 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  TimerStream('Ola mundo', Duration(seconds: 2)).listen(print);
+  BehaviorSubject<String> stcA = new BehaviorSubject<String>();
+  BehaviorSubject<String> stcB = new BehaviorSubject<String>.seeded('Mundo');
+
+  stcA.stream.pipe(stcB);
+  stcA.sink.add('Ola');
+
+  stcA.stream.listen((e) => print('Dados da stream A: $e'));
+  stcB.stream.listen((e) => print('Dados da stream B: $e'));
 }
