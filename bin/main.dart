e1deced86363992db2ca6d6241ccc674982f4951
pauloantonelli <pauloantonelli@hotmail.com>
2020-01-11 14:15:54 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -3,13 +3,5 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  RangeStream lista = new RangeStream(1, 50);
-
-  RangeStream(1, 50)
-      .switchMap((item) {
-        print('switch: $item');
-        return new TimerStream((item * 2), Duration(seconds: 2));
-      })
-      .debounce((e) => TimerStream(true, Duration(seconds: 3)))
-      .listen((e) => print(e));
+  TimerStream('Ola mundo', Duration(seconds: 2)).listen(print);
 }
