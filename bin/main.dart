c8490ff0e3cdb37cac07450096eec1e152877d89
pauloantonelli <pauloantonelli@hotmail.com>
2020-01-11 12:18:05 -0300

    fim

--- bin/main.dart
+++ bin/main.dart
@@ -6,29 +6,9 @@ void main() {
   StreamController stc = new StreamController.broadcast();
 
   stc.stream
-      .handleError(_handleError, test: meuTeste)
-      .asyncMap((e) => fakeRequest(e))
+      .debounce((e) => TimerStream(true, Duration(seconds: 10)))
       .listen((onData) => print('Stream: $onData'));
 
-  stc.sink.add('o');
-}
-
-void _handleError(error, stackTrace) {
-  print('Erro: $error');
-}
-
-bool meuTeste(erro) {
-  if (erro is String) {
-    return true;
-  }
-  return false;
-}
-
-Future<List<String>> fakeRequest(String query) async {
-  print('query recebida: $query');
-  await Future.delayed(Duration(microseconds: 500));
-  // throw 'Deu Erro'; // gera erro na stream
-  return ['Jão', 'Paulo', 'Carlos', 'Daniel', 'Romário']
-      .where((e) => e.contains(query))
-      .toList();
+  stc.sink.add(1);
+  stc.sink.add(5);
 }
